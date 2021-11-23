d6056a20d535614b26d00e677c27805fcb9c51eb
adamcg123 <adam.Gondorchin26@gmail.com>
2021-11-23 17:51:54 -0600

    got restaurants working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,61 @@
-function initGoogle() {
-    var location = {
-        lat: 40.000,
-        lng: -79.000
-    }
-    var options = {
-        center: location,
-        zoom: 9
-    } 
-    if(navigator.geolocation) {
-        console.log("geolocation is here!");
-    
-        navigator.geolocation.getCurrentPosition((loc) => {
-            location.lat = loc.coords.latitude;
-            location.lng = loc.coords.longitude;
+let map= null;
 
-            map = new google.maps.Map(document.getElementById("map"), options);
-        },
-        (err) => {
-            console.log("User clicked no");
-            map = new google.maps.Map(document.getElementById("map"), options);
+function initMap(){
+    navigator.geolocation.getCurrentPosition(function(pos){
+        location.lat = pos.coords.latitude;
+        location.long = pos.coords.longitude;
+        map = new google.maps.Map(document.getElementById('map'),{
+
+            center: {lat: location.lat, lng: location.long },
+            zoom: 15
+        });
+        getRestaurants(location);
+    });
+}
+
+
+function getRestaurants(location) {
+    var pyrmont = new google.maps.LatLng(location.lat,location.long);
+    var request = {
+        location: pyrmont,
+        radius: '1500',
+        type: ['restaurant']
+    };
+service = new google.maps.places.PlacesService(map);
+service.nearbySearch(request, callback);
+}
+
+function callback (results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            console.log(results)
+            var place = results[i];
+            let price = createPrice(place.price_level);
+            let content = `<h3>${place.name}</h3>
+            <h4>${place.vicinity}</h4>
+            <p>Price: ${price}<br/>
+            Rating: ${place.rating}`;
+
+            var marker = new google.maps.Marker({
+                position: place.geometry.location,
+                map: map,
+                title: place.name
+            });
+
+            var infowindow = new google.maps.infowindow({
+                content: content
+            })
+
+
         }
-        )
-    }else {
-        console.log("geolocation not supported :(");
-        map = new google.maps.Map(document.getElementById("map"), options);
     }
 
-    autocomplete = new google.maps.places.Autocomplete(document.getElementById("input"),
-    {
-        componentResstrictions: {'country': ['us']},
-        feilds: ['geometry', 'name'],
-        types: ['establishment']
-    })
 
-    autocomplete.addListener("place_changed", () => {
-        var place = autocomplete.getPlace();
-        new google.maps.Marker({
-            position: place.geometry.location,
-            title: place.name,
-            map: map
 
-        })
-    })
+
+
+
+
 
 
 }
